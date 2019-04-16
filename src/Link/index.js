066a94491fff36b19925d2469d109d8b2418409c
Eric Laursen <ericjlaursen@gmail.com>
2019-04-16 10:02:19 -0400

    Link component

---
+++ src/Link/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+const Link = (props) => {
+	return(<a href={props.address}>Click</a>)
+}
+export default Link;
---
+++ src/Link/index.test.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Link from "./index"
+import { configure, shallow } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+
+configure({ adapter: new Adapter() })
+
+describe('<Link />', () => {
+	it('passes address PROP to href', () => {
+		const linkWrapper = shallow(<Link address={'www.google.com'} />)
+		expect(linkWrapper.find('a[href="www.google.com"]').exists()).toBe(true)
+	})
+})
