6d3e7a2a945017c258f5f2e4f51a75057e287c06
Eric Laursen <ericjlaursen@gmail.com>
2019-04-16 11:54:45 -0400

    tests CHANGING conditional props display value

--- src/Link/index.test.js
+++ src/Link/index.test.js
@@ -16,4 +16,20 @@ describe('<Link />', () => {
 		const linkWrapper = shallow(<Link address={'www.google.com'} tOrF={false}/>)
 		expect(linkWrapper.find('a[href="www.google.com"]').length).toBe(0)
 	})
+
+	it('Updates From null to visible on conditional prop change', () => {
+		const linkWrapper = shallow(<Link address={'www.google.com'} tOrF={false}/>)
+		expect(linkWrapper.find('a[href="www.google.com"]').length).toBe(0)
+
+		linkWrapper.setProps({tOrF: true})
+		expect(linkWrapper.find('a[href="www.google.com"]').length).toBe(1)
+	})
+
+	it('Updates From visible to null on conditional prop change', () => {
+		const linkWrapper = shallow(<Link address={'www.google.com'} tOrF={true}/>)
+		expect(linkWrapper.find('a[href="www.google.com"]').length).toBe(1)
+
+		linkWrapper.setProps({tOrF: false})
+		expect(linkWrapper.find('a[href="www.google.com"]').length).toBe(0)
+	})
 })
