4f61df6e31c5d524a8845240f5e944715bd70af5
Eric Laursen <ericjlaursen@gmail.com>
2019-04-16 11:50:05 -0400

    includes example app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const App = () => {
-  return (
-    <div>
-      <p>Dummy React Component here!</p>
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Title from './Title/'
+import Link from './Link'
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header className="App-header">
+          <h1 alt="dummyAlt" className="App-title" id="h1ID">Testing Overview</h1>
+        </header>
+        <Title text="dummy title"/>
+        <Link address={'dummyLinkText'} tOrF={true} />
+        <ul className="myClass">
+          <li>List Item 1</li>
+          <li>List Item 2</li>
+          <li>List Item 3</li>
+        </ul>
+        <p className="App-intro">
+          Mic Check?!
+        </p>
+      </React.Fragment>
+    );
+  }
+}
+
 export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
